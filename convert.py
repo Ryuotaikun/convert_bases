bea52486ce67ea8c6f69edcce22abaf02e3f111a
Martin Piepgras <piepgras.martin@gmail.com>
2018-04-04 20:00:11 +0200

    Convert from every base <= 10 to every base

--- convert.py
+++ convert.py
@@ -2,20 +2,42 @@ import math
 
 def __main__():
     inputString = input("Please insert the number you want to convert: ")
-    inputStartingBase = input("In what system do you want to convert: ")
-    inputNumber = int(inputString)
-    base = int(inputStartingBase)
+    inputStartingBase = input("What base is your number (number from 2-10): ")
+    inputWantedBase = input("In what base do you want to convert: ")
 
-    convertedNumberString = convertIntoBase(base, inputNumber)
+    numberToConvert = int(inputString)
+    startingBase = int(inputStartingBase)
+    wantedBase = int(inputWantedBase)
 
-    print ("Decimal: " + inputString)
-    print (str(base) + "-base:  " + convertedNumberString)
+    print (str(startingBase) + "-base:  " + inputString)
 
-def convertIntoBase(baseInteger, number):
+    if (startingBase != 10):
+        numberToConvert = convertIntoDecimal(startingBase, numberToConvert)
+
+    if (wantedBase != startingBase):
+        if (wantedBase != 10):
+            print ("Decimal: " + str(numberToConvert))
+            convertedNumberString = convertIntoBase(wantedBase, numberToConvert)
+        else:
+            convertedNumberString = str(numberToConvert)
+        print (str(wantedBase) + "-base:  " + convertedNumberString)
+    else:
+        print ("not very usefull converting into the same system, is it?")
+
+def convertIntoDecimal(base, number):
+    numberString = str(number)
+    decimalNumber = 0
+    power = len(numberString)-1
+    for digit in numberString:
+        decimalNumber += int(digit)*base**power
+        power -= 1
+    return decimalNumber
+
+def convertIntoBase(base, number):
     generatedList = []
     while (number > 0):
-        generatedList.append(number % baseInteger)
-        number = math.floor(number / baseInteger)
+        generatedList.append(number % base)
+        number = math.floor(number / base)
     newNumberString = ""
     for digit in reversed(generatedList):
         if digit < 10:
