589e488017ea371be2dea8e9d28285b62718fe55
Martin Piepgras <piepgras.martin@gmail.com>
2018-04-04 19:33:31 +0200

    Convert from decimal into every other base (as long as there are letters to display it)

--- convert.py
+++ convert.py
@@ -2,25 +2,26 @@ import math
 
 def __main__():
     inputString = input("Please insert the number you want to convert: ")
-    inputBase = input("In what system do you want to convert (number from 2 - 9) :")
+    inputStartingBase = input("In what system do you want to convert: ")
     inputNumber = int(inputString)
-    base = int(inputBase)
+    base = int(inputStartingBase)
 
-    convertedNumber = convertIntoBase(base, inputNumber)
+    convertedNumberString = convertIntoBase(base, inputNumber)
 
     print ("Decimal: " + inputString)
-    print (str(base) + "-base:  " + str(convertedNumber))
+    print (str(base) + "-base:  " + convertedNumberString)
 
 def convertIntoBase(baseInteger, number):
     generatedList = []
     while (number > 0):
         generatedList.append(number % baseInteger)
         number = math.floor(number / baseInteger)
-    newNumber = 0
-    position = 0
-    for digit in generatedList:
-        newNumber += digit*(10**position)
-        position += 1
-    return newNumber
+    newNumberString = ""
+    for digit in reversed(generatedList):
+        if digit < 10:
+            newNumberString += chr(48+digit)
+        else:
+            newNumberString += chr(55+digit)
+    return newNumberString
 
 __main__()
